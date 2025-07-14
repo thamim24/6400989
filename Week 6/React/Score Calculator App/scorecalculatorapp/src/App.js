d786c15142393d2718075ec5863b6ef4eba8870f
thamim24 <thamimul2004@gmail.com>
2025-07-14 10:34:47 +0530

    Week 6

---
+++ Week 6/React/Score Calculator App/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name="Thamimul Ansari"
+        School="SIHS School"
+        total={556}
+        goal={6}
+      />
+    </div>
+  );
+}
+
+export default App;
