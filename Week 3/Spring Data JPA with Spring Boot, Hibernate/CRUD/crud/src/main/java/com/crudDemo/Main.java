4150a5bb12d9437223d230496dbf2d762ed54a6f
Thamimul Ansari <thamimul2004@gmail.com>
2025-08-18 23:14:07 +0530

    Update Main.java

--- Week 3/Spring Data JPA with Spring Boot, Hibernate/CRUD/crud/src/main/java/com/crudDemo/Main.java
+++ Week 3/Spring Data JPA with Spring Boot, Hibernate/CRUD/crud/src/main/java/com/crudDemo/Main.java
@@ -7,20 +7,32 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Crud crud=new Crud();
-        crud.setId(13);
-        crud.setName("Mary");
-        crud.setDept("ECE");
+        crud.setId(12);
+        crud.setName("Doe");
+        crud.setDept("CSE");
 
         SessionFactory sessionFactory=new Configuration().
                                       configure().
                                       addAnnotatedClass(com.crudDemo.Crud.class).
                                       buildSessionFactory();
         Session session=sessionFactory.openSession();
-        session.beginTransaction();
-        session.merge(crud);
-
+//        session.beginTransaction();
+//        session.persist(crud);
+//        session.getTransaction().commit();
+        crud = session.find(com.crudDemo.Crud.class, 11);
+        System.out.println(crud);
+        crud = session.find(com.crudDemo.Crud.class, 12);
+        System.out.println(crud);
         crud = session.find(com.crudDemo.Crud.class, 13);
         System.out.println(crud);
+//        session.merge(crud);
+//        session.getTransaction().commit();
+//        crud = session.find(com.crudDemo.Crud.class, 13);
+
+//        Crud crud1 = session.find(com.crudDemo.Crud.class, 12);
+//        session.remove(crud1);
+//        session.getTransaction().commit();
+//        System.out.println(crud1);
         session.close();
         sessionFactory.close();
     }
